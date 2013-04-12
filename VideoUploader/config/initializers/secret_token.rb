aa59fe643798483092e2f81f1e6b42968ca935d6
Miroslav Spassov <miroslavspassov@yahoo.com>
2013-04-11 20:37:32 -0700

    First commit

---
+++ VideoUploader/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VideoUploader::Application.config.secret_token = '4107e559abee28d21f1518d927c4546ba19171222c94308bcc726cb1087b200dca07bea5abab00bc14dd6a07716cd4fab2dc21f8c2a0f15d73bb9f2b3c25bba0'
