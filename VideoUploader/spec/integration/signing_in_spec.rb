6e7af6bbc9dad4281ccc2c76e73d2c66ecfa26f4
John Higginson <jhigginson@gmail.com>
2013-05-14 16:31:20 -0700

    Add feature for signing in via the Devise-provided form

--- VideoUploader/spec/integration/signing_in_spec.rb
+++ VideoUploader/spec/integration/signing_in_spec.rb
@@ -12,4 +12,14 @@ feature 'Signing in' do
     page.should have_content("Signed in as videouploader@example.com")
   end
 
+  scenario 'Signing in via form' do
+    User.find_by_email('videouploader@example.com').confirm!
+    visit '/'
+    click_link 'Sign in'
+    fill_in 'Email', with: "videouploader@example.com"
+    fill_in 'Password', with: "password"
+    click_button "Sign in"
+    page.should have_content("Signed in successfully.")
+  end
+
 end
