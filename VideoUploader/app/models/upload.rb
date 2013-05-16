0020a0822041d4e135c77c79e84172e8bd627406
Miroslav Spassov <miroslavspassov@yahoo.com>
2013-05-15 20:29:49 -0700

    Added fileupload with carrierwave

--- VideoUploader/app/models/upload.rb
+++ VideoUploader/app/models/upload.rb
@@ -1,4 +1,5 @@
 class Upload < ActiveRecord::Base
-  attr_accessible :description, :name
+  attr_accessible :description, :name, :file_name
   validates_presence_of :name, :description, message: "is required field and can't be blank!"
+  mount_uploader :file_name,  FileUploader
 end
--- VideoUploader/db/schema.rb
+++ VideoUploader/db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130512201047) do
+ActiveRecord::Schema.define(:version => 20130516024308) do
 
   create_table "uploads", :force => true do |t|
     t.string   "name"
     t.string   "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.string   "file_name"
   end
 
 end
